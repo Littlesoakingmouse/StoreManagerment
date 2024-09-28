edf511174a19fc6f224922cc4e958a0a051e60d0
Littlesoakingmouse <ntrungquan2005@gmail.com>
2024-09-28 20:51:42 +0700

    push Controller

--- src/main/java/com/example/storemanager/controller/ProductController.java
+++ src/main/java/com/example/storemanager/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.storemanager.controller;
 
 import com.example.storemanager.dto.request.ProductRequestCreateDTO;
+import com.example.storemanager.dto.request.ProductRequestUpdateDTO;
 import com.example.storemanager.entity.Product;
 import com.example.storemanager.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +21,30 @@ public class ProductController {
 
     @PostMapping("create")
     public ResponseEntity<?> create(@RequestBody ProductRequestCreateDTO dto) {
-        productService.addProduct();
-
+        productService.addProduct(dto);
+        return ResponseEntity.ok("Create product successfully");
     }
 
+    @PutMapping("update")
+    public ResponseEntity<?> update(@RequestBody ProductRequestUpdateDTO dto) {
+        productService.updateProduct(dto);
+        return ResponseEntity.ok("Update product successfully");
+    }
+
+    @DeleteMapping("delete")
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+        productService.deleteProductById(id);
+        return ResponseEntity.ok("Delete product successfully");
+    }
+
+    @GetMapping("detail/{id}")
+    public ResponseEntity<?> detail(@PathVariable Integer id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @GetMapping("find/{number}")
+    public ResponseEntity<?> find(@PathVariable Integer number) {
+        return ResponseEntity.ok(productService.FindxthLagestNumber(number));
+    }
 
 }
